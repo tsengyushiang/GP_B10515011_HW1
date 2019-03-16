40108fe7045addb323911f29c8bf0816346ca4a4
tsengyushiang <tsengyushiang@gmail.com>
2019-03-17 01:43:42 +0800

    V2
    修正audio source

---
+++ Assets/mirrorCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mirrorCamera : MonoBehaviour
+{
+    public Transform target;
+    public Transform obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        var deltPos=obj.transform.position - target.transform.position;
+        transform.position = target.transform.position+ new Vector3(deltPos.x, deltPos.y, -deltPos.z);
+
+    }
+}
