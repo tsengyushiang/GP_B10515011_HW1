2babe32ddf3163da590f4eda96a4cf94aee0cb4f
tsengyushiang <tsengyushiang@gmail.com>
2019-03-16 23:37:25 +0800

    Initial commit

---
+++ Assets/gameControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class gameControl : MonoBehaviour
+{
+    public Light GlobalLight ;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void replay() {
+        SceneManager.LoadScene(0);
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
